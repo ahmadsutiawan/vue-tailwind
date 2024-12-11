43fb647551e46e063f7cd9e71c587445a7b9b866
ahmadsutiawan <ythahmadsutiawan@gmail.com>
2024-12-11 17:11:40 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -22,4 +22,5 @@ export default {
 
 <style lang="">
 
+
 </style>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,15 +1,16 @@
-<template lang="">
-  <nav class="flex justify-between items-center p-5">
-    <div class="flex flex-row uppercase">
-      <h1 class="text-3xl font-bold">Ahmad</h1>
-      <h1 class="text-3xl text-blue-800 font-light ml-3">Sutiawan</h1>
+<template>
+  <nav class="items-center  flex flex-row">
+    <div class="w-1/2 flex items-center bg-slate-100">
+      <h1 class="text-3xl font-bold text-white">Ahmad</h1>
+      <h1 class="text-3xl font-light text-blue-500 ml-3">Sutiawan</h1>
     </div>
-    <div>
-      <ul class="flex space-x-4">
-        <li>Home</li>
-        <li>About</li>
-        <li>Work</li>
-        <li>Contact</li>
+
+    <div class="w-1/2 flex content-end bg-slate-600">
+      <ul class="flex space-x-4 text-white">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Work</a></li>
+        <li><a href="#">Contact</a></li>
       </ul>
     </div>
   </nav>
